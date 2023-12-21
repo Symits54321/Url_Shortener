22c3ea5222384df80fcafb5e2c80de58279e08f1
Symits54321 <125802160+Symits54321@users.noreply.github.com>
2023-12-21 14:33:10 +0530

    logged in basic funtionality done

--- controllers/api/usercontroller_api.js
+++ controllers/api/usercontroller_api.js
@@ -2,7 +2,7 @@ const userModel = require('../../models/user_model');
 const shortUrlModel = require('../../models/shorturl_model');
 
 
-// to view all list of products
+// registration
 module.exports.registration = async function (req, res) {
 
     console.log("Registration req is");
@@ -10,6 +10,24 @@ module.exports.registration = async function (req, res) {
 
   
     try {
+        
+        // check if exists
+        let oldUser = await userModel.find({
+            username:req.query.username,
+            password:req.query.password
+        });
+
+    if(oldUser){
+
+            return res.status(200).json({
+
+                        message: 'Already registered, please login',
+                        data:oldUser
+                    
+                    }); 
+             }  
+
+
 
         //create
         let newUser = await userModel.create({
@@ -27,14 +45,72 @@ module.exports.registration = async function (req, res) {
                     });
 
 
+        }else{
+
+            return res.status(500).json({
+
+                message: 'registration Unsuccesfull',
+                data:req.query
+            
+            }); 
         }
        
 
     }catch (error) {
 
         console.error('Error in registration:', error);
-        res.status(500).send('Error in registration ');
+        res.status(500).json({
 
+            message: 'registration Unsuccesfull',
+            wrongurl:`/user${req.url}`,
+            advice:'please use the correct url, example given below',
+            correcturlexample: '/user/registration/?username=abcd&password=12345',
+            error:error
+        }); 
+    }
+
+}
+
+// login
+module.exports.login = async function (req, res) {
+
+    try {
+
+            let user = await userModel.find({
+                username:req.query.username,
+                password:req.query.password
+            });
+
+
+    // if registered
+     if (user.username){
+            return res.status(200).json({
+
+                message: 'login successfull',
+                data:user
+            
+            });
+
+        }else{
+
+            res.status(500).json({
+
+                message: 'login Unsuccesfull , please register',
+                
+            }); 
+        }
+        
+    } catch (error) {
+
+          console.error('Error in login:', error);
+          res.status(500).json({
+
+            message: 'login Unsuccesfull /error',
+            wrongurl:`/user${req.url}`,
+            advice:'please register / use the correct url, example given below',
+            correcturlexample: '/user/login/?username=abcd&password=12345',
+            error:error
+        }); 
     }
 
 }
