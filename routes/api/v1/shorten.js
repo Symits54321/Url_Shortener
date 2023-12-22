0e0239e6dcca04a64e4bef1187edb383f6e14e41
Symits54321 <125802160+Symits54321@users.noreply.github.com>
2023-12-22 15:29:53 +0530

    stage 1 completed:(basic all  functionality in local environment)

--- routes/api/v1/shorten.js
+++ routes/api/v1/shorten.js
@@ -5,7 +5,6 @@ const router = express.Router();
 
 
  router.get('/:url',urlcontroller.shorten); // to shorten
-// router.post('/newshorturl',usercontroller.shortUrlRender); // to renderUrl
-//  router.get('/logout',usercontroller.logout); // to logout
+
 
 module.exports=router;
