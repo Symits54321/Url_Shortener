002bb8b96ef9169692fa346ba7a63170eaa6fbad
Symits54321 <125802160+Symits54321@users.noreply.github.com>
2023-12-28 14:06:42 +0530

    netlify config 2

--- functions/api.js
+++ functions/api.js
@@ -5,51 +5,51 @@ const router = express.Router();
 
 let records = [];
 
-router.use('/',require('../routes/index'));
-// router.get('/', (req, res) => {
-//     res.send('App is running..');
-//   });
+//router.use('/',require('../routes/index'));
+router.get('/', (req, res) => {
+    res.send('App is running..');
+  });
   
-// //Get all students
-// router.get('/', (req, res) => {
-//   res.send('App is running..');
-// });
+//Get all students
+router.get('/', (req, res) => {
+  res.send('App is running..');
+});
 
-// //Create new record
-// router.post('/add', (req, res) => {
-//   res.send('New record added.');
-// });
+//Create new record
+router.post('/add', (req, res) => {
+  res.send('New record added.');
+});
 
-// //delete existing record
-// router.delete('/', (req, res) => {
-//   res.send('Deleted existing record');
-// });
+//delete existing record
+router.delete('/', (req, res) => {
+  res.send('Deleted existing record');
+});
 
-// //updating existing record
-// router.put('/', (req, res) => {
-//   res.send('Updating existing record');
-// });
+//updating existing record
+router.put('/', (req, res) => {
+  res.send('Updating existing record');
+});
 
-// //showing demo records
-// router.get('/demo', (req, res) => {
-//   res.json([
-//     {
-//       id: '001',
-//       name: 'Smith',
-//       email: 'smith@gmail.com',
-//     },
-//     {
-//       id: '002',
-//       name: 'Sam',
-//       email: 'sam@gmail.com',
-//     },
-//     {
-//       id: '003',
-//       name: 'lily',
-//       email: 'lily@gmail.com',
-//     },
-//   ]);
-// });
+//showing demo records
+router.get('/demo', (req, res) => {
+  res.json([
+    {
+      id: '001',
+      name: 'Smith',
+      email: 'smith@gmail.com',
+    },
+    {
+      id: '002',
+      name: 'Sam',
+      email: 'sam@gmail.com',
+    },
+    {
+      id: '003',
+      name: 'lily',
+      email: 'lily@gmail.com',
+    },
+  ]);
+});
 
 app.use('/.netlify/functions/api', router);
 module.exports.handler = serverless(app);
